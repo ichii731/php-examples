25d96dc8d9610f14190e300664d48cf145690bfe
tomox0115 <it0731@outlook.jp>
2021-06-20 06:27:12 +0900

    fix 404

--- laravel_error-page/resources/views/errors/404.blade.php
+++ laravel_error-page/resources/views/errors/404.blade.php
@@ -1,11 +1,5 @@
 @extends('errors.layouts.base')
-
 @section('http-request', '404')
-
 @section('title', 'Page not found')
-
 @section('message', 'This may not mean anything.')
-{{-- 該当アドレスのページを見つける事ができませんでした。 --}}
-
 @section('detail', "I'm probably working on something that has blown up.")
-{{-- サーバーは要求されたリソースを見つけることができなかったことを示します。 URLのタイプミス、もしくはページが移動または削除された可能性があります。 トップページに戻るか、もう一度検索してください。 --}}
